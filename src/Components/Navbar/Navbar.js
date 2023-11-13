bd62091db1eae8b10235844482d1c6570d97f595
Suyukee <suyukee@ya.ru>
2023-11-13 23:26:36 +0300

    Interface

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,24 +3,24 @@ import s from './Navbar.module.css'
 
 const Navbar = () => {
 	return (
-		<nav className={ s.nav }>
-			<div className={ s.item }>
+		<nav className={s.nav}>
+			<div className={s.item}>
 				<NavLink to="/profile">Profile</NavLink>
 			</div>
-			<div className={ s.item }>
+			<div className={s.item}>
 				<NavLink to="/messages">Messages</NavLink>
 			</div>
-			<div className={ s.item }>
+			<div className={s.item}>
 				<NavLink to="/news">News</NavLink>
 			</div>
-			<div className={ s.item }>
+			<div className={s.item}>
 				<NavLink to="/music">Music</NavLink>
 			</div>
-			<div className={ s.item }>
+			<div className={s.item}>
 				<NavLink to="/settings">Settings</NavLink>
 			</div>
 		</nav>
 	);
 }
- 
+
 export default Navbar;
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -2,11 +2,14 @@ import s from './Profile.module.css'
 
 const Profile = () => {
 	return (
-		<div className={ s.content }>
-			<h3>Profile</h3>
-			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+		<div className={s.content}>
+			<img src="https://sun59-1.userapi.com/s/v1/if2/SKDQfyY1NFSQrjgFJMrXfF3hjdv5rOAQ_aJTXI7EPN7G6fzGyXu8wmCAsgAy3FE5O6DncfQiR4eUhOvdv74n9z7H.jpg?quality=95&crop=65,56,703,703&as=50x50,100x100,200x200,400x400&ava=1&u=pYyra2gbPNUJtPhJw-YJKUV8V0EZwL5uJHo3ubWIswY&cs=200x200" />
+			<div className={ s.name }>
+				<h3>Kozyrev Vladimir</h3>
+				<p>My status</p>
+			</div>
 		</div>
 	);
 }
- 
+
 export default Profile;
