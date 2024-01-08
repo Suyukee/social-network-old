74520cf76adaeb516b63ffedf882c26c888ce8b6
Suyukee <suyukee@ya.ru>
2024-01-08 19:17:23 +0300

    Added users-reselect

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setCurrentPage, getUsers, follow, unfollow } from '../../redux/usersReducer';
+import { setCurrentPage, requestUsers, follow, unfollow } from '../../redux/usersReducer';
 import Users from './Users';
 import Preloader from '../common/Preloader/Preloader';
+import {
+	getUsers,
+	getPageSize,
+	getTotalUsersCount,
+	getCurrentPage,
+	getIsFetching,
+	getFollowingInProgress,
+} from '../../redux/users-selectors';
 
 class UsersContainer extends React.Component {
 	componentDidMount() {
-		this.props.getUsers(this.props.currentPage, this.props.pageSize);
+		this.props.requestUsers(this.props.currentPage, this.props.pageSize);
 	}
 	onPageChanged = (pageNumber) => {
 		this.props.setCurrentPage(pageNumber);
-		this.props.getUsers(pageNumber, this.props.pageSize);
+		this.props.requestUsers(pageNumber, this.props.pageSize);
 	};
 	render() {
 		return (
@@ -31,20 +39,31 @@ class UsersContainer extends React.Component {
 	}
 }
 
+// let mapStateToProps = (state) => {
+// 	return {
+// 		users: state.usersPage.users,
+// 		pageSize: state.usersPage.pageSize,
+// 		totalUsersCount: state.usersPage.totalUsersCount,
+// 		currentPage: state.usersPage.currentPage,
+// 		isFetching: state.usersPage.isFetching,
+// 		followingInProgress: state.usersPage.followingInProgress,
+// 	};
+// };
+
 let mapStateToProps = (state) => {
 	return {
-		users: state.usersPage.users,
-		pageSize: state.usersPage.pageSize,
-		totalUsersCount: state.usersPage.totalUsersCount,
-		currentPage: state.usersPage.currentPage,
-		isFetching: state.usersPage.isFetching,
-		followingInProgress: state.usersPage.followingInProgress,
+		users: getUsers(state),
+		pageSize: getPageSize(state),
+		totalUsersCount: getTotalUsersCount(state),
+		currentPage: getCurrentPage(state),
+		isFetching: getIsFetching(state),
+		followingInProgress: getFollowingInProgress(state),
 	};
 };
 
 let mapDispatchToProps = {
 	setCurrentPage,
-	getUsers,
+	requestUsers,
 	follow,
 	unfollow,
 };
